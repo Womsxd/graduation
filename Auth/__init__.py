d4b6bc1d9e3a4adf821f79d74c8cf0eb77173bbe
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-09 22:19:38 +0800

    set uid

--- Auth/__init__.py
+++ Auth/__init__.py
@@ -47,6 +47,7 @@ def login():
         db.session.commit()
         # 提交修改到数据库
         login_user(user)
+        session["_uid"] = user.id
         return json.dumps({'code': 0, "message": ""})
     return json.dumps({'code': 1, "message": "Account/Password Error"})
 
