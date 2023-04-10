4526022cb7245d65f17c11e4e860d7399322558c
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-10 22:30:34 +0800

    用户添加&修改&删除

--- user/__init__.py
+++ user/__init__.py
@@ -1,5 +1,6 @@
 import auth.utils
 from database import models, db
+from group import check_permissions
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 
@@ -17,5 +18,54 @@ def changepwd():
         return jsonify({'code': 1, "message": "Password Error"})
     user.password = auth.utils.get_password(new_pwd)
     user.csrf = None
+    db.session.commit()
+    return jsonify({'code': 0, "message": ""})
+
+
+@userf.route('/user/add', methods=['post'])
+@login_required
+@check_permissions(1)
+def add():
+    account = request.form.get('account')
+    password = request.form.get('password')
+    groupid = request.form.get('groupid')
+    if account is None and password is None:
+        return jsonify({'code': 3, "message": "Account/Password Not equal to None"})
+    user = models.User()
+    user.account = account
+    user.password = auth.autils.get_password(password)
+    if groupid is not None:
+        user.groupid = groupid
+    db.session.commit()
+    return jsonify({'code': 0, "message": ""})
+
+
+@userf.route('/user/edit', methods=['post'])
+@login_required
+@check_permissions(1)
+def edit():
+    account = request.form.get('account')
+    if account is None:
+        return jsonify({'code': 3, "message": "Account/Password Not equal to None"})
+    password = request.form.get('password')
+    groupid = request.form.get('groupid')
+    user = models.User.query.filter_by(account=account).first()
+    if password is not None:
+        user.password = auth.utils.get_password(password)
+        user.csrf = None
+    if groupid is not None:
+        user.groupid = groupid
+    db.session.commit()
+    return jsonify({'code': 0, "message": ""})
+
+
+@userf.route('/user/delete', methods=['post'])
+@login_required
+@check_permissions(1)
+def delete():
+    account = request.form.get('account')
+    if account is None:
+        return jsonify({'code': 3, "message": "Account Not equal to None"})
+    models.User.query.filter_by(account=account).delete()
     db.session.commit()
     return jsonify({'code': 0, "message": ""})
