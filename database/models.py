1e70f9b0d48dd4143c5642519d1d3067ed64e985
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-24 08:26:56 +0800

    配置外键约束

--- database/models.py
+++ database/models.py
@@ -5,45 +5,82 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Clas(db.Model):
-    __tablename__ = 'class'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text(20), nullable=False)
-    college_id = db.Column(db.Integer, db.ForeignKey('college.id'), nullable=False, server_default=text("1"))
-
-    college = db.relationship('College', backref='class')
-
-
-class College(db.Model):
+class College(db.Model):  # 学院
     __tablename__ = 'college'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(20), nullable=False)
 
 
-class Examinfo(db.Model):
-    __tablename__ = 'examinfo'
+class Clas(db.Model):  # 班级
+    __tablename__ = 'class'
 
     id = db.Column(db.Integer, primary_key=True)
-    sid = db.Column(db.Text, db.ForeignKey('student.sid'), nullable=False)
-    sessions_id = db.Column(db.Integer, db.ForeignKey('examsessions.id'), nullable=False, server_default=text("1"))
-    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False, server_default=text("1"))
-    result = db.Column(db.Float, nullable=False)
+    name = db.Column(db.Text(20), nullable=False)
+    college_id = db.Column(db.Integer, db.ForeignKey('college.id', ondelete='SET DEFAULT'), nullable=False
+                           , server_default=text("1"))
 
-    student = db.relationship('Student', backref='examinfo')
-    sessions = db.relationship('Examsession', backref='examinfo')
-    subject = db.relationship('Subject', backref='examinfo')
+    college = db.relationship('College', backref='class')
+
+    __table_args__ = (
+        {'mysql_engine': 'InnoDB'}
+    )
+
+    # https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#orm-declarative-table-configuration
 
 
-class Examsession(db.Model):
+class Examsession(db.Model):  # 考试信息
     __tablename__ = 'examsessions'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(30))
 
 
-class Group(db.Model):
+class Subject(db.Model):  # 科目
+    __tablename__ = 'subject'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text(30), nullable=False)
+
+
+class Student(db.Model):  # 学生信息
+    __tablename__ = 'student'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sid = db.Column(db.Text, nullable=False, unique=True)
+    name = db.Column(db.Text(20), nullable=False)
+    sex = db.Column(db.Integer, nullable=False, server_default=text("1"))
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id', ondelete='SET DEFAULT'),
+                         nullable=False, server_default=text("1"))
+
+    _class = db.relationship('Clas', backref='student')
+
+    __table_args__ = (
+        {'mysql_engine': 'InnoDB'}
+    )
+
+
+class Examinfo(db.Model):  # 考试成绩
+    __tablename__ = 'examinfo'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sid = db.Column(db.Text, db.ForeignKey('student.sid', ondelete='cascade'), nullable=False)
+    sessions_id = db.Column(db.Integer, db.ForeignKey('examsessions.id', ondelete='SET DEFAULT'),
+                            nullable=False, server_default=text("1"))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id', ondelete='SET DEFAULT'),
+                           nullable=False, server_default=text("1"))
+    result = db.Column(db.Float, nullable=False)
+
+    student = db.relationship('Student', backref='examinfo')
+    sessions = db.relationship('Examsession', backref='examinfo')
+    subject = db.relationship('Subject', backref='examinfo')
+
+    __table_args__ = (
+        {'mysql_engine': 'InnoDB'}
+    )
+
+
+class Group(db.Model):  # 权限组
     __tablename__ = 'groups'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -51,26 +88,7 @@ class Group(db.Model):
     inherit = db.Column(db.Text())
 
 
-class Student(db.Model):
-    __tablename__ = 'student'
-
-    id = db.Column(db.Integer, primary_key=True)
-    sid = db.Column(db.Text, nullable=False, unique=True)
-    name = db.Column(db.Text(20), nullable=False)
-    sex = db.Column(db.Integer, nullable=False, server_default=text("1"))
-    class_id = db.Column(db.Integer, db.ForeignKey('class.id'), nullable=False, server_default=text("1"))
-
-    _class = db.relationship('Clas', backref='student')
-
-
-class Subject(db.Model):
-    __tablename__ = 'subject'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text(30), nullable=False)
-
-
-class User(db.Model):
+class User(db.Model):  # 用户组
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -80,3 +98,7 @@ class User(db.Model):
     csrf = db.Column(db.Text, unique=True)
 
     group = db.relationship('Group', backref='user')
+
+    __table_args__ = (
+        {'mysql_engine': 'InnoDB'}
+    )
