21f248ddeed30b35439e29631e4a47dc9dc2232b
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-25 15:10:51 +0800

    考试信息 √

--- exam/session.py
+++ exam/session.py
@@ -1,3 +1,4 @@
+import utils
 import messages
 from exam import exam
 from database import models, db
@@ -15,9 +16,7 @@ def add():
     name = request.form.get('name')
     if not (id_ and name):
         return jsonify(messages.DATA_NONE)
-    esession = models.Examsession()
-    esession.id = id_
-    esession.name = name
+    esession = models.Examsession(id=id_,name=name)
     try:
         with db.session.begin():
             db.session.add(esession)
@@ -70,11 +69,55 @@ def delete():
 @exam.route('/exam/session/list', methods=['GET', 'POST'])
 @login_required
 @check_permissions(1)
-def ulist():
+def get_list():
     page = request.values.get("page", 1, type=int)
-    pagination = models.Examsession.query.paginate(page=page, per_page=20)
+    querying = models.Examsession.query
+    search = request.values.get("search")
+    if search is not None:
+       querying = querying.filter(models.Examsession.name.like(f"%{search}%"))
+    pagination  = querying.paginate(page=page, per_page=20)
     sessions = [{"id": i.id, "name": i.name} for i in pagination.items]
     data = {"sessions": sessions, "total": pagination.total, "current": page, "maximum": pagination.pages}
     returns = {"data": data}
     returns.update(messages.OK)
     return jsonify(returns)
+
+@exam.route('/exam/session/query', methods=['GET', 'POST'])
+@login_required
+@check_permissions(2)
+def query():
+    id_ = request.values.get("id")
+    if id_ is None:
+        return jsonify(messages.DATA_NONE)
+    esession = models.Examsession.query.filter_by(id=id_).first()
+    if esession is None:
+        return jsonify(messages.DATA_NONE)
+    returns = {"data": {"id": esession.id, "name": esession.name}}
+    returns.update(messages.OK)
+    return jsonify(returns)
+
+@exam.route('/exam/session/import_xls', methods=['POST'])
+@login_required
+@check_permissions(1)
+def import_xls():
+    file = request.files.get('file')
+    if file is None:
+        return jsonify(messages.DATA_NONE)
+    result = utils.load_xls_file(file.read(), "考试信息")
+    if result is None:
+        return jsonify(messages.NOT_XLS_FILE)
+    if not result:
+        return jsonify(messages.XLS_NAME_ERROR)
+    if ["考试信息"] != result[0][:1]:
+        return jsonify(messages.XLS_TITLE_ERROR)
+    if len(result[1:]) == 0:
+        return jsonify(messages.XLS_IMPORT_EMPTY)
+    try:
+        with db.session.begin()[1:]:
+            for i in result:
+                esession = models.Examsession(name=i[0])
+                db.session.add(esession)
+        return jsonify(messages.OK)
+    except SQLAlchemyError:
+        db.session.rollback()
+        return jsonify(messages.DATABASE_ERROR)
