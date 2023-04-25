a7415332efebb28b41288a8bd5206ef3741cd282
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-25 10:01:00 +0800

    otp full

--- user/__init__.py
+++ user/__init__.py
@@ -1,10 +1,12 @@
+import time
+import pyotp
 import utils
 import messages
 from database import models, db
 from group import check_permissions
 from sqlalchemy.exc import SQLAlchemyError
-from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
+from flask import Blueprint, request, jsonify, session
 
 userf = Blueprint('userf', __name__)
 
@@ -21,6 +23,74 @@ def change_password():
         return jsonify(messages.PASSWORD_ERROR)  # 就这里使用password提示
     user.password = utils.get_password(new_pwd)
     user.csrf = None
+    try:
+        db.session.commit()
+        return jsonify(messages.OK)
+    except SQLAlchemyError:
+        db.session.rollback()
+        return jsonify(messages.DATABASE_ERROR)
+
+
+@userf.route('/user/otp/enable', methods=['POST'])
+@login_required
+def otp_enable():
+    user = models.User.query.filter_by(id=session["_uid"]).first()
+    if user.otp_status == 1:
+        return jsonify(messages.OTP_OPEN)
+    user.otp_secret = pyotp.random_base32()
+    otp_fast_url = pyotp.totp.TOTP(user.otp_secret). \
+        provisioning_uri(name=user.account, issuer_name='Graduation of Womsxd')
+    user.otp_act_exp_time = int(time.time()) + 300
+    try:
+        db.session.commit()
+        returns = {"data": {"otp": {"secret": user.otp_secret, "qrcode": utils.get_b64_qrcode(otp_fast_url),
+                                    "expiration": user.otp_act_exp_time}}}
+        returns.update(messages.OK)
+        return jsonify(returns)
+    except SQLAlchemyError:
+        db.session.rollback()
+        return jsonify(messages.DATABASE_ERROR)
+
+
+@userf.route('/user/otp/verify', methods=['POST'])
+@login_required
+def otp_verify():
+    otp_code = request.form.get("otp_code")
+    if not otp_code:
+        return jsonify(messages.DATA_NONE)
+    user = models.User.query.filter_by(id=session["_uid"]).first()
+    if user.otp_status == 1:
+        return jsonify(messages.OTP_OPEN)
+    if user.otp_secret is None or user.otp_act_exp_time is None:
+        return jsonify(messages.OTP_NEED_OPEN)
+    if int(time.time()) > user.otp_act_exp_time:
+        return jsonify(messages.OTP_TIME_OUT)
+    if not pyotp.TOTP(user.otp_secret).verify(otp_code):
+        return jsonify(messages.OTP_VERIFY_ERROR)
+    user.otp_status = 1
+    user.otp_act_exp_time = None
+    try:
+        db.session.commit()
+        return jsonify(messages.OK)
+    except SQLAlchemyError:
+        db.session.rollback()
+        return jsonify(messages.DATABASE_ERROR)
+
+
+@userf.route('/user/otp/disable', methods=['POST'])
+@login_required
+def otp_disable():
+    otp_code = request.form.get("otp_code")
+    if not otp_code:
+        return jsonify(messages.DATA_NONE)
+    user = models.User.query.filter_by(id=session["_uid"]).first()
+    if user.otp_status == 0 or user.otp_secret is None:
+        return jsonify(messages.OTP_NEED_OPEN)
+    if not pyotp.TOTP(user.otp_secret).verify(otp_code):
+        return jsonify(messages.OTP_VERIFY_ERROR)
+    user.otp_status = 0
+    user.otp_secret = None
+    user.otp_act_exp_time = None
     try:
         db.session.commit()
         return jsonify(messages.OK)
