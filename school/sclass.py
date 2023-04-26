8350b0a3516222450cf6d96878cb449e3dcbf9df
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-26 13:46:03 +0800

    班级管理 √

--- school/sclass.py
+++ school/sclass.py
@@ -12,15 +12,16 @@ from sqlalchemy.exc import SQLAlchemyError
 @login_required
 @check_permissions(1)
 def add():
-    cid = request.form.get('id')
     name = request.form.get('name')
-    college = request.form.get('college')
-    if not (cid and name and college):
+    if name is None:
         return jsonify(messages.DATA_NONE)
-    class_ = models.Clas()
-    class_.id = cid
-    class_.name = name
-    class_.college = college
+    class_ = models.Clas(name=name)
+    college = request.form.get('college')
+    if college is None:
+        class_.college_id = college
+    grade = request.form.get("grade")
+    if grade is None:
+        class_.grade = grade
     try:
         with db.session.begin(nested=True):
             db.session.add(class_)
@@ -41,11 +42,14 @@ def edit():
     if class_ is None:
         return jsonify(messages.NOT_FOUND)
     name = request.form.get('name')
-    college = request.form.get('college')
     if name is not None:
         class_.name = name
+    college = request.form.get('college')
     if college is not None:
         class_.college = college
+    grade = request.form.get("grade")
+    if grade is None:
+        class_.grade = grade
     try:
         db.session.commit()
         return jsonify(messages.OK)
@@ -73,13 +77,25 @@ def delete():
         return jsonify(messages.DATABASE_ERROR)
 
 
+@school.route('/school/class/grades', methods=['GET', 'POST'])
+@login_required
+@check_permissions(1)
+def get_grades():
+    page = request.values.get("page", 1, type=int)
+    pagination = db.session.query(models.Clas.grade).distinct().paginate(page=page, per_page=20)
+    data = {"grades": pagination.items, "total": pagination.total, "current": page, "maximum": pagination.pages}
+    returns = {"data": data}
+    returns.update(messages.OK)
+    return jsonify(returns)
+
+
 @school.route('/school/class/list', methods=['GET', 'POST'])
 @login_required
 @check_permissions(1)
 def get_list():
     page = request.values.get("page", 1, type=int)
     querying = models.Student.query.join(models.Clas).with_entities(
-        models.Clas.id, models.Clas.name, models.College.name.label('college_n')
+        models.Clas.id, models.Clas.name, models.Clas.grade, models.College.name.label('college_n')
     )
     search = request.values.get("search")
     if search is not None:
@@ -87,8 +103,11 @@ def get_list():
     college_id = request.values.get("college_id")
     if college_id is not None:
         querying = querying.filter_by(college_id=college_id)
+    grade = request.form.get("grade")
+    if grade is None:
+        querying = querying.filter_by(grade=grade)
     pagination = querying.paginate(page=page, per_page=20)
-    classes = [{"id": i.id, "name": i.name, "college_name": i.college_n}
+    classes = [{"id": i.id, "name": i.name, "grade": i.grade, "college_name": i.college_n}
                for i in pagination.items]
     data = {"classes": classes, "total": pagination.total, "current": page, "maximum": pagination.pages}
     returns = {"data": data}
@@ -104,11 +123,11 @@ def query():
     if cid is None:
         return jsonify(messages.DATA_NONE)
     class_ = models.Clas.query.join(models.College).with_entities(
-        models.Clas.id, models.Clas.name, models.College.name.label("college_n")
+        models.Clas.id, models.Clas.name, models.Clas.grade, models.College.name.label("college_n")
     ).filter_by(id=cid).first()
     if class_ is None:
         return jsonify(messages.DATA_NONE)
-    returns = {"data": {"id": class_.id, "name": class_.name, "college": class_.college_n}}
+    returns = {"data": {"id": class_.id, "name": class_.name, "grade": class_.grade, "college": class_.college_n}}
     returns.update(messages.OK)
     return jsonify(returns)
 
@@ -125,25 +144,26 @@ def import_xls():
         return jsonify(messages.NOT_XLS_FILE)
     if not result:
         return jsonify(messages.XLS_NAME_ERROR)
-    if ["班级名称", "所属学院"] != result[0][:2]:
+    if ["学院", "年级", "班级名称"] != result[0][:2]:
         return jsonify(messages.XLS_TITLE_ERROR)
     if len(result[1:]) == 0:
         return jsonify(messages.XLS_IMPORT_EMPTY)
     try:
         with db.session.begin(nested=True):
             college_cache = {}
+            class_adds = []
             for i in result[1:]:
-                college_id = 1  # 默认学院id
-                if i[1]:
-                    if i[1] not in college_cache.keys():
+                college_id = None
+                if i[0]:
+                    if i[0] not in college_cache.keys():
                         college = models.College.query.filter_by(name=i[1]).first()
                         if college:
                             college_id = college.id
-                        college_cache[i[1]] = college_id
+                        college_cache[i[0]] = college_id
                     else:
-                        college_id = college_cache[i[1]]
-                class_ = models.Clas(name=i[0], collede_id=college_id)
-                db.session.add(class_)
+                        college_id = college_cache[i[0]]
+                class_adds.append(models.Clas(name=i[2], collede_id=college_id, grade=i[1]))
+            db.session.bulk_save_objects(class_adds)
         return jsonify(messages.OK)
     except SQLAlchemyError:
         db.session.rollback()
