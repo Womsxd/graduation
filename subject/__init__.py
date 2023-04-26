947ad5238b2de19c3193e34f7ab428a34bf2e8ce
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-26 21:42:46 +0800

    加入去空行

--- subject/__init__.py
+++ subject/__init__.py
@@ -105,7 +105,7 @@ def import_xls():
     file = request.files.get('file')
     if file is None:
         return jsonify(messages.DATA_NONE)
-    result = utils.load_xls_file(file.read(), "科目")
+    result = [i for i in utils.load_xls_file(file.read(), "科目") if i != ""]
     if result is None:
         return jsonify(messages.NOT_XLS_FILE)
     if not result:
